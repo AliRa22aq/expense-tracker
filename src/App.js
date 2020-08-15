2968f54a540fff24a7e863a377a4f59ac9258351
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-15 13:32:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import {Header} from './components/Header'
 import {Balance} from './components/Balance'
 import {AccountSummary} from './components/AccountSummary'
+import {TransectionHistory} from './components/TransectionHistory'
+
 
 function App() {
   return (
     <div>
-      <Header /> 
-      <div className="container"> 
-      <Balance />
-      </div>
-      <AccountSummary />
-    </div>
+            <Header /> 
+        <div className="container"> 
+            <Balance />
+        </div>
+        <div> 
+             <AccountSummary />
+        </div>
+        <div> 
+             <TransectionHistory />
+        </div>
+     </div>
   );
 }
 
