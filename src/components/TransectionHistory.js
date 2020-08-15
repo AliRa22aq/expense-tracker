df01463e24a74e8693ce5b2131d2671c862f77a3
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-15 13:32:39 +0500

    Update TransectionHistory.js

---
+++ src/components/TransectionHistory.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const TransectionHistory = () => {
+    return (
+        <div>
+            <h3> Transection History </h3>
+            <ul className="list"> 
+                <li className="minus"> 
+                    Project1: Income
+                    <span> $1000 </span> 
+                </li>
+                <li className="plus"> 
+                Project1: Salaries
+                <span> -$500 </span>
+                </li>  
+            </ul>
+            
+        </div>
+    )
+}
