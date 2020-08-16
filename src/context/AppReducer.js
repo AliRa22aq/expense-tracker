1d7a6394d90ac3c5933f8b4efa7714ce84016284
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-16 09:50:50 +0500

    Update AppReducer.js

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,20 @@
 export default (state, action) => {
     switch(action.type) {
+        case 'DELETE_TRANSECTION':
+            return {
+                ...state,
+                transections: [action.payload, ...state.transections]
+
+            }
+        
+        case 'ADD_TRANSECTION':
+            return {
+                ...state,
+                transections: state.transections
+                    .filter(transection => transection.id !== action.payload)
+
+            }    
+
         default:
             return state;
     }
