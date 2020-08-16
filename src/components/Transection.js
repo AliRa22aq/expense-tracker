8c91c196ff6347d9babfe44cd2d836031d6b217e
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-16 09:50:53 +0500

    Update Transection.js

--- src/components/Transection.js
+++ src/components/Transection.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transection = ({transection}) => {
+
+    const {delTransection} = useContext(GlobalContext)
 
     const sign = transection.transectionAmount >0 ? '+':'-';
     const transectionType = transection.transectionAmount > 0 ? "plus":"minus";
@@ -10,7 +13,9 @@ export const Transection = ({transection}) => {
         <li className={transectionType}> 
             {transection.description}
             <span> {sign}${Math.abs(transection.transectionAmount)} </span> 
-            <button className="delete-btn">X</button>
+            <button className="delete-btn"
+                    onClick={()=> delTransection(transection.id)}
+                    >X</button>
         </li>
 
     )
